f59fb605d8f55e95504d602dbf79bc67689e186b
gtpitch <bmontagu@gmail.com>
2018-01-07 21:12:55 -0500

    msm8998: add back overlays with support in AOSiP
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- common.mk
+++ common.mk
@@ -137,7 +137,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
-    Snap
+    SnapdragonCamera \
+    Camera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
