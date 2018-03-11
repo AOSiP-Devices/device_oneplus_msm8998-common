0d072e59c4b878832493abeee0e1bedc6c25712d
mrhainavn <mrhainavn@gmail.com>
2018-03-12 06:10:33 +0700

    dumpling: import doze package from op3
    Signed-off-by: mrhainavn <mrhainavn@gmail.com>

--- common.mk
+++ common.mk
@@ -187,6 +187,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-service.widevine
+ 
+# Doze mode
+PRODUCT_PACKAGES += \
+    OneplusThreeDoze
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
