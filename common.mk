723bb3b9e2d8382b64e295dc587ebec30fdf5b9b
Brett Montague <bmontagu@gmail.com>
2018-04-08 13:56:55 -0400

    msm8998: build snap and add padding overlay
    Change-Id: I651d135da11ac29ef5998f46538b214e02cbb0d0
    Signed-off-by: Brett Montague <bmontagu@gmail.com>

--- common.mk
+++ common.mk
@@ -162,7 +162,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service.oneplus5 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    SnapdragonCamera
+    Snap
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
