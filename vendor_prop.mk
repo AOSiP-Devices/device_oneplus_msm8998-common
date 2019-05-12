2c2286a44b7828456ccf1981efb0cb9899ed4615
David Sn <divad.nnamtdeis@gmail.com>
2019-05-12 14:07:01 +0200

    msm8998-common: Add extra props for VoLTE
    Co-authored-by: neobuddy89 <neobuddy89@gmail.com>
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -227,14 +227,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.bar_fake_gcell=1 \
     persist.vendor.radio.arfcn_test_mode=3
 
-# Radio - VoLTE / VoWifi
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.ims_volte_enable=1 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.radio.calls.on.ims=1
-
 # RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rmnet.data.enable=true \
@@ -277,6 +269,32 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # VNDK
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.vndk.version=28
+
+# VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.ims.dropset_feature=0 \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.radio.custom_ecc=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.ignore_dom_time=10 \
+    persist.radio.rat_on=combine \
+    persist.radio.sib16_support=1 \
+    persist.radio.RATE_ADAPT_ENABLE=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.is_wps_enabled=true \
+    persist.radio.videopause.mode=1 \
+    persist.radio.sap_silent_pin=1 \
+    persist.radio.always_send_plmn=true \
+    persist.rcs.supported=0 \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.calls.on.ims=1
 
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
