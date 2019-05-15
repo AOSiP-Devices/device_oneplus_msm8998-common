0b59799f45ceb0fa1288ad02bd2c86d94d47ab38
David Sn <divad.nnamtdeis@gmail.com>
2019-05-15 15:48:47 +0200

    msm8998-common: Add a KeyHandler for tri-state-key events
     * BoardConfig: Remove oppo/common
     * aosip.dependencies: Replace oppo_common with devicesettings
    
    Change-Id: I22a7ee5b7eec5667b14f1a587a6d0138e77dc0f3
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- common.mk
+++ common.mk
@@ -263,6 +263,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# KeyHandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fpc1020.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/fpc1020.kl \
@@ -485,6 +489,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
-
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
