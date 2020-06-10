736a71697a334898c88469f93b112a7b7f7dc5f7
Omkar Chandorkar <forumomkar@gmail.com>
2020-06-10 07:29:25 +0530

    Add Dalvik properties back device seems to OOM itself at boot

--- common.mk
+++ common.mk
@@ -167,6 +167,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=8m
+
 # Device Settings
 PRODUCT_PACKAGES += \
     DeviceSettings
